86363155ddfaca82ce9e3a09b71965ff6d92340c
root <root@sojourner.mayfirst.org>
2010-06-15 23:19:53 -0400

    Contrib modules folder and he date module in it (downloaded because no release, not even dev release, tagged for D7-- requires patch, applied and included in folder, to avoid parse error.

---
+++ drupal/sites/default/modules/date/date_popup/date_popup.js
@@ -0,0 +1,34 @@
+// $Id: date_popup.js,v 1.4 2010/03/01 01:33:02 karens Exp $
+
+/**
+ * Attaches the calendar behavior to all required fields
+ */
+(function ($) {
+Drupal.behaviors.date_popup = {
+  attach: function (context) {
+  for (var id in Drupal.settings.datePopup) {
+    $('#'+ id).bind('focus', Drupal.settings.datePopup[id], function(e) {
+      if (!$(this).hasClass('date-popup-init')) {
+        var datePopup = e.data;
+        // Explicitely filter the methods we accept.
+        switch (datePopup.func) {
+          case 'datepicker':
+            $(this)
+              .datepicker(datePopup.settings)
+              .addClass('date-popup-init')
+              .focus();
+            break;
+
+          case 'timeEntry':
+            $(this)
+              .timeEntry(datePopup.settings)
+              .addClass('date-popup-init')
+              .focus();
+            break;
+        }
+      }
+    });
+  }
+  }
+};
+})(jQuery);
