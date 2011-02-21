3cd193fbc1e37877aa5f604e8638e748192f7298
mlncn <ben@agaric.com>
2011-02-21 01:33:48 -0800

    Updates to menu_block, references, link, and gravatar.

--- drupal/sites/all/modules/menu_block/README.txt
+++ drupal/sites/all/modules/menu_block/README.txt
@@ -12,6 +12,8 @@ CONFIGURING MENU BLOCKS
 When adding or configuring a menu block, several configuration options are
 available:
 
+Basic Options:
+
 Block title
   For menu trees that start with the 1st level, the default block title will be
   the menu name. For menu trees that start with the 2nd level or deeper, the
@@ -34,24 +36,25 @@ Administrative title
   unique title to be used on that page. If blank, the regular title will be
   used.
 
-Parent item
-  First select the menu. Then select the parent item from that menu. The tree of
-  links will only contain children of the selected parent item.
+Menu name
+  Select the menu to use for the tree of links.
 
 Starting level
   Blocks that start with the 1st level will always be visible. Blocks that start
   with the 2nd level or deeper will only be visible when the trail to the active
   menu item is in the block's tree.
 
-Make the starting level follow the active menu item
-  If the active menu item is deeper than the level specified above, the starting
-  level will follow the active menu item. Otherwise, the starting level of the
-  tree will remain fixed.
-
 Maximum depth
   From the starting level, specify the maximum depth of the tree. Blocks with a
   maximum depth of 1 will just be a single un-nested list of links with none of
   those links' children displayed.
+
+Advanced options:
+
+Make the starting level follow the active menu item
+  If the active menu item is deeper than the level specified above, the starting
+  level will follow the active menu item. Otherwise, the starting level of the
+  tree will remain fixed.
 
 Expand
   All children of this menu will be expanded.
@@ -60,6 +63,11 @@ Sort
   Sort each item in the active trail to the top of its level. When used on a
   deep or wide menu tree, the active menu item's children will be easier to see
   when the page is reloaded.
+
+Fixed parent item
+  If you select a specific menu item, you alter the "starting level" and
+  "maximum depth" options to be relative to the fixed parent item. The tree of
+  links will only contain children of the selected parent item.
 
 
 STYLING MENU BLOCKS
--- drupal/sites/all/modules/menu_block/menu-block.js
+++ drupal/sites/all/modules/menu_block/menu-block.js
@@ -8,35 +8,6 @@ Drupal.behaviors.menu_block = {
     }
     $('#menu-block-settings', context).addClass('menu-block-processed');
 
-    // Split the un-wieldly "parent item" pull-down into two hierarchal pull-downs.
-    $('.menu-block-parent', context)
-      .html(Drupal.settings.menu_block.parent_options[Drupal.settings.menu_block.menus_default])
-      .val(Drupal.settings.menu_block.parent_default)
-      .before(Drupal.settings.menu_block.menus)
-      .wrap(Drupal.settings.menu_block.parent_wrapper)
-      .before(Drupal.settings.menu_block.parent_label);
-    $('.menu-block-parent-menu', context).change( function() {
-      $('.menu-block-parent')
-        .html(Drupal.settings.menu_block.parent_options[$('.menu-block-parent-menu').val()])
-        .val(Drupal.settings.menu_block.parent_default);
-    } );
-    if ($('.menu-block-parent-menu').size()) {
-      $('.form-item-parent>label').text(Drupal.settings.menu_block.menus_label_text);
-    }
-
-    // Toggle display of "menu parent" based on display options.
-    $('input[name=display_options]', context).change( function() {
-      if ($('input[name=display_options]:checked').val() == 'advanced') {
-        $('.menu-block-parent-wrapper').slideDown('fast');
-      }
-      else {
-        $('.menu-block-parent-wrapper').slideUp('fast');
-      }
-    } );
-    if ($('input[name=display_options]:checked', context).val() == 'basic') {
-      $('.menu-block-parent-wrapper', context).css('display', 'none');
-    }
-
     // Show the "display options" if javascript is on.
     $('.form-item-display-options.form-type-radios>label', context).addClass('element-invisible');
     $('.form-item-display-options.form-type-radios', context).show();
@@ -46,9 +17,18 @@ Drupal.behaviors.menu_block = {
     // Override the default show/hide animation for Form API states.
     $('#menu-block-settings', context).bind('state:visible', function(e) {
       if (e.trigger) {
-        e.stopPropagation() /* Stop the handler further up the tree. Note: test this as there could be problems */
+        e.stopPropagation() /* Stop the handler further up the tree. */
         $(e.target).closest('.form-item, .form-wrapper')[e.value ? 'slideDown' : 'slideUp']('fast');
       }
+    });
+
+    // Syncronize the display of menu and parent item selects.
+    $('.menu-block-parent-mlid', context).change( function() {
+      var menuItem = $(this).val().split(':');
+      $('.menu-block-menu-name').val(menuItem[0]);
+    });
+    $('.menu-block-menu-name', context).change( function() {
+      $('.menu-block-parent-mlid').val($(this).val() + ':0');
     });
   }
 };
