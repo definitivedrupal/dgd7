952a21ac00498297fb33e7cd1976bb1c7daa867f
Benjamin Melançon <ben@agaric.com>
2014-07-24 09:07:05 -0400

    Update Follow module

--- web/sites/all/modules/follow/follow.api.php
+++ web/sites/all/modules/follow/follow.api.php
@@ -23,3 +23,33 @@ function hook_follow_networks_alter(&$networks, $uid = 0) {
     'domain' => 'identi.ca',
   );
 }
+
+/**
+ * Alter the available icon styles.
+ *
+ * @param array $styles
+ *   An array of icon styles.
+ */
+function hook_follow_icon_styles_alter(&$styles) {
+  $styles['my-style'] = array(
+    'name' => 'my-style',
+    'label' => t('My Custom Icon Style'),
+    // An array of CSS overrides. These  overrides will only apply to the
+    // generic anchor (a.follow-link). If you would like something more
+    // customized than this, it is recommended you add it to your own custom
+    // stylesheet, or use the CSS Injector module. NOTE: if you change the CSS
+    // overrides after the style is already in use, you will have to delete the
+    // generated CSS file in your public files directory.
+    'css-overrides' => array(
+      'height: 44px;',
+      'line-height: 40px;',
+      'padding-left: 41px;',
+      'padding-right: 5px;',
+    ),
+    // You can specify a custom path to your icons. Your icons must be named
+    // appropriately in order for them to work. Please see the small or large
+    // icons in the follow module directory for an example.
+    'icon-path' => 'sites/all/themes/mytheme/follow-icons',
+  );
+
+}
